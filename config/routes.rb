bb6ee20b8b60b24b10a0bdb7703eca475dbc9dbe
Methal Abeywickrama <methal.abeywickrama@icloud.com>
2024-04-03 11:52:38 +0530

    update articles resource, create comments resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   root "articles#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles do
+    resources :comments
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
